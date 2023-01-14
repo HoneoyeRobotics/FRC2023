e855e412f01000f971366ae6167a8d174d3061ef
honeoyerobotics3951 <36572163+honeoyerobotics3951@users.noreply.github.com>
2023-01-14 11:17:22 -0500

    Wired Up commands and subsytems

--- FRC2023/src/main/java/frc/robot/RobotContainer.java
+++ FRC2023/src/main/java/frc/robot/RobotContainer.java
@@ -6,9 +6,16 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import frc.robot.subsystems.DriveTrain;
 
 public class RobotContainer {
+
+  public DriveTrain driveTrain;
+
+
   public RobotContainer() {
+    driveTrain = new DriveTrain();
+
     configureBindings();
   }
 
