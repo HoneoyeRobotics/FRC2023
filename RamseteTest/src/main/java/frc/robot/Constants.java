ba94840232d0f22c25bc744af1e0f9a51a591c8a
honeoyerobotics3951 <36572163+honeoyerobotics3951@users.noreply.github.com>
2023-01-25 20:24:30 -0500

    merge sysid data

--- RamseteTest/src/main/java/frc/robot/Constants.java
+++ RamseteTest/src/main/java/frc/robot/Constants.java
@@ -41,9 +41,9 @@ public final class Constants {
     // for *your* robot's drive.
     // The Robot Characterization Toolsuite provides a convenient tool for obtaining these
     // values for your robot.
-    public static final double ksVolts = 0.22;
-    public static final double kvVoltSecondsPerMeter = 1.98;
-    public static final double kaVoltSecondsSquaredPerMeter = 0.2;
+    public static final double ksVolts = 0.16478;
+    public static final double kvVoltSecondsPerMeter = 2.2025;
+    public static final double kaVoltSecondsSquaredPerMeter = 0.16441;
 
     // Example value only - as above, this must be tuned for your drive!
     public static final double kPDriveVel = 8.5;
