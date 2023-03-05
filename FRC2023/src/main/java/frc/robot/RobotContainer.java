d161731090686fb9cfe2de5f83363f7276c44034
honeoyerobotics3951 <36572163+honeoyerobotics3951@users.noreply.github.com>
2023-03-05 16:15:49 -0500

    Added ScoringPos selector

--- FRC2023/src/main/java/frc/robot/RobotContainer.java
+++ FRC2023/src/main/java/frc/robot/RobotContainer.java
@@ -4,24 +4,21 @@
 
 package frc.robot;
 
+import edu.wpi.first.wpilibj.shuffleboard.*;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.button.CommandJoystick;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
-import frc.robot.commands.BrakeMode;
-import frc.robot.commands.DriveUntilPerpendicular;
-import frc.robot.commands.MoveToScorePos;
-import frc.robot.commands.ResetNavX;
-import frc.robot.commands.RotateToDegree;
-import frc.robot.commands.TeleopDrive;
-import frc.robot.subsystems.Arms;
-import frc.robot.subsystems.DriveTrain;
-import frc.robot.subsystems.Vision;
+import frc.robot.commands.*;
+import frc.robot.enums.*;
+import frc.robot.subsystems.*;
 public class RobotContainer {
   private DriveTrain driveTrain;
   private Arms arms;
   private Vision vision;
-  CommandXboxController driverJoystick = new CommandXboxController(0);
+  private CommandXboxController driverJoystick = new CommandXboxController(0);
+  private CommandJoystick buttonBoard = new CommandJoystick(1);
 
   public RobotContainer() {
     driveTrain = new DriveTrain();
@@ -35,6 +32,8 @@ public class RobotContainer {
     () -> driverJoystick.getHID().getLeftBumper()));
 
     configureBindings();
+    configureButtonBoard();
+    initializeScorePosition();
     SmartDashboard.putData("ResetNavX", new ResetNavX(driveTrain));
   }
 
@@ -44,6 +43,31 @@ public class RobotContainer {
     driverJoystick.b().onTrue(new RotateToDegree(driveTrain, 0));
     driverJoystick.start().onTrue(new MoveToScorePos(driveTrain, vision));
   }
+
+  private void configureButtonBoard() {
+
+        //Changes the scoring position grid on dashboard
+        buttonBoard.axisGreaterThan(1, .5).onTrue(new ChangeScoringHeight(arms, false));
+        buttonBoard.axisLessThan(1, -.5).onTrue(new ChangeScoringHeight(arms, true));
+    
+        buttonBoard.axisGreaterThan(0, .5).onTrue(new ChangeScoringSlot(arms, true));
+        buttonBoard.axisLessThan(0, -.5).onTrue(new ChangeScoringSlot(arms, false));
+  }
+
+  private void initializeScorePosition() {
+    ShuffleboardTab tab2 = Shuffleboard.getTab("Tab 2");
+    int i;      
+    String l_position;
+    for(i = 1; i < 10; ++i) {
+      l_position = String.format("ScorePos%d%s", i, ScoringHeight.High.toString());
+      tab2.add(l_position, false).withPosition(i - 1, 0);
+      l_position = String.format("ScorePos%d%s", i, ScoringHeight.Med.toString());
+      tab2.add(l_position, false).withPosition(i - 1, 1);
+      l_position = String.format("ScorePos%d%s", i, ScoringHeight.Low.toString());
+      tab2.add(l_position, false).withPosition(i - 1, 2);
+    }
+  }
+  
   public Command getAutonomousCommand() {
     return Commands.print("No autonomous command configured");
   }
--- FRC2023/src/main/java/frc/robot/subsystems/Arms.java
+++ FRC2023/src/main/java/frc/robot/subsystems/Arms.java
@@ -4,19 +4,88 @@
 
 package frc.robot.subsystems;
 
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableInstance;
+//import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.AnalogInput;
-import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
+import frc.robot.enums.ScoringHeight;
 
 public class Arms extends SubsystemBase {
   private AnalogInput proxSensor;
+  private ScoringHeight scoringHeight = ScoringHeight.Low;
+  private int scoringSlot = 1;    
+  private String l_position;
+  
+  private NetworkTableInstance table = NetworkTableInstance.getDefault();
+  private NetworkTable myTable = table.getTable("Shuffleboard/Tab 2");
   //private DigitalInput proxSensor;
   /** Creates a new Arm. */
   public Arms() {
     proxSensor = new AnalogInput(Constants.proxSensorID);
     //proxSensor = new DigitalInput(Constants.proxSensorID);
+  }
+
+  public void changeScoringHeight(boolean up) {
+    switch(scoringHeight) {
+      case Low: 
+        if (up) 
+          scoringHeight = ScoringHeight.Med;
+        break;
+      case Med:
+        if(up) 
+          scoringHeight = ScoringHeight.High;
+        else 
+          scoringHeight = ScoringHeight.Low;
+        break;
+      case High:
+        if(!up) 
+          scoringHeight = ScoringHeight.Med;
+        break;
+      default:
+        scoringHeight = ScoringHeight.Med;
+        break;
+    }
+    SmartDashboard.putString("ScoringHeight", scoringHeight.toString());
+    smartDashboardScorePosition();
+  }
+
+  public ScoringHeight getScoringHeight() {
+    return scoringHeight;
+  }
+
+  public void changeScoringSlot(boolean right) {
+    if(right) {
+      if(scoringSlot == 9)
+        scoringSlot = 1;
+      else 
+        ++scoringSlot;
+    }
+    else {
+      if(scoringSlot == 1) 
+        scoringSlot = 9; 
+      else
+        --scoringSlot;
+    }
+    SmartDashboard.putNumber("Scoring Slot", scoringSlot);
+    smartDashboardScorePosition();
+  }
+
+  public int getScoringSlot() {
+    return scoringSlot;
+  }
+
+  public void smartDashboardScorePosition() {
+    //NetworkTableEntry myEntry = myTable.getEntry("ScorePos1High");
+    //myEntry.setBoolean(true);
+
+    if (l_position != null) {
+      myTable.getEntry(l_position).setBoolean(false);
+    }
+    l_position = String.format("ScorePos%d%s", scoringSlot, scoringHeight.toString());
+      myTable.getEntry(l_position).setBoolean(true);
   }
 
   @Override
